e76085fcf2d3edda71edf907d2543d90a929e9ef
zuoshao_glb <youxiudiwo@github.com>
2019-09-03 09:27:52 +0800

    测试文件提交

---
+++ src/main/java/com/weaver/wevaer/WevaerApplication.java
@@ -0,0 +1,13 @@
+package com.weaver.wevaer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WevaerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WevaerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/weaver/wevaer/heihei.java
@@ -0,0 +1,8 @@
+package com.weaver.wevaer;
+
+/**
+ * @author zuoshao
+ * @date 2019/7/20 - 23:00
+ */
+public class heihei {
+}
